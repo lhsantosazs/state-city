133e34c17366d11091d12b42243804b45ebc8a95
Luiz Henrique Santos <lhsantosazs@gmail.com>
2021-03-02 22:48:47 -0300

    Organizing state requests

---
+++ src/app/Http/Requests/State/ListStateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\State;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ListStateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'numeric|exists:state,id'
+        ];
+    }
+
+    /**
+     * Return validation errors as json response
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = ['Validation errors' => $validator->errors()];
+
+        throw new HttpResponseException(response()->json($errors, 400));
+    }
+}
